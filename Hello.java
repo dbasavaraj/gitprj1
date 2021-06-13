19bfeeb6c4a9caa430b4744216fa5e036ce9d4ab
Basavaraj <diddegeb@gmail.com>
2021-06-13 23:41:57 +0530

    changed

--- Hello.java
+++ Hello.java
@@ -3,7 +3,9 @@ public class Hello{
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Test Chandana");
-		System.out.println("God is Greate");
+		System.out.println("God is Great");
+		System.out.println("God is Great");
+		System.out.println("God is Great");
 	}
 
 }
